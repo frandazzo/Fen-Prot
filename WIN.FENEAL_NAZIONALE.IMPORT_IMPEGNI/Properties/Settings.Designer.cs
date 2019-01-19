4f88b715c0fe9eaa2f39d836c2788fb20958c526
Francesco Randazzo <fg.randazzo@hotmail.it>
2019-01-19 19:46:40 +0100

    aggiornamnto devexpress

--- WIN.FENEAL_NAZIONALE.IMPORT_IMPEGNI/Properties/Settings.Designer.cs
+++ WIN.FENEAL_NAZIONALE.IMPORT_IMPEGNI/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace WIN.FENEAL_NAZIONALE.IMPORT_IMPEGNI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,7 @@ namespace WIN.FENEAL_NAZIONALE.IMPORT_IMPEGNI.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://www.fenealgest.it/servizi/WebServices/SharetopIntegration.asmx")]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://www.fenealweb.it/servizi/WebServices/SharetopIntegration.asmx")]
         public string WIN_FENEAL_NAZIONALE_IMPORT_IMPEGNI_localhost_SharetopIntegration {
             get {
                 return ((string)(this["WIN_FENEAL_NAZIONALE_IMPORT_IMPEGNI_localhost_SharetopIntegration"]));
